b2b347a60de6640dfc384ed0826f2498c6a59986
Sujit Kumar Libi <sujit_libi@hotmail.com>
2026-02-15 19:38:11 +0545

    Jumping Letter Project added

---
+++ project-7-jumping-letter/script.js
@@ -0,0 +1,7 @@
+const letters = document.querySelectorAll('.letters span');
+
+letters.forEach((letter) => {
+  letter.addEventListener('click', (e) => {
+    e.target.classList.add('active');
+  });
+});
